64c70a9c23f7e4b74371030f611f8ad698ad7e2e
Paulo Ricardo Acílio Chimelo <54581313+prac0402@users.noreply.github.com>
2020-03-09 21:50:25 -0300

    Update qualitativa.js

--- login-beta/js/qualitativa.js
+++ login-beta/js/qualitativa.js
@@ -1,3 +1,16 @@
-function entrar(){
-    
+let lista = []
+
+
+function selectionsort(lista. inicio = 0){
+    if (inicio > lista.length - 1 ){
+        return  //condição de saida
+    }
+    let pasmin = inicio + 1
+    for(let i= pasmin + 1; i < lista.length; i++)
+        if(Menor (lista.[pasmin] lista[i]) pasmin = i){
+
+        }
+        if(lista[pasmin]< lista[inicio])troca(lista[pasmin], lista[inicio]){
+            selectionsort(vet, inicio + 1)
+        }
 }
