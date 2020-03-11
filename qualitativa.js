45074e7a02efee2ca6c8e46674bbb2147e3f3533
Paulo Ricardo Acílio Chimelo <54581313+prac0402@users.noreply.github.com>
2020-03-10 21:10:39 -0300

    Add files via upload

---
+++ qualitativa.js
@@ -0,0 +1,19 @@
+function teste(){
+
+let adc = document.getElementById("valores").value;
+let lista = [adc]
+
+let xmax = lista[lista.length - 1];
+let xmin = lista[0]
+let at = xmax-xmin
+
+let k = lista.length
+k = (Math.sqrt(k))
+let k2 = k.tofixed(0)
+
+alert(k2,k)
+}
+
+
+
+
