d19188f02bddd98a67b384a36f669331c08b9915
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-05-19 08:24:21 -0300

    Update func.js

--- func.js
+++ func.js
@@ -32,7 +32,7 @@ function quantitativa(){
         if (tipoquanti == 'DISCRETA'){
             console.log('34')
         }else if (tipoquanti == 'NOMINAL'){
-            
+            quantitativacontinua()
         }
         }else if (tipodados == 'POPULAÇÃO'){
         if (tipoquanti == 'DISCRETA'){
@@ -43,20 +43,41 @@ function quantitativa(){
     }
 }
 
-
+function ordemCrescente(a,b){
+    return(a-b)	
+}   
 function qualitativadiscreta(){
-    console.log('Teste')
+    tablequalitativa.innerHTML +="<tr> <td>VARIAVEL</td><td>FI</td><td>FR%   </td><td>FAC   </td><td>FAC%  </td></tr> "
+    var nomeVar = document.getElementById('variavelquali').value; 
+    var dados = document.getElementById('valor').value; 
+    let vetDiscreta = dados.toString().split(';'); 
+    vetDiscreta.sort(ordemCrescente) 
+    var quantDados ={} ; 
+    var acum =0 ; 
+    variavelquali.innerHTML+=nomeVar
+        for(let i = 0 ; i<vetDiscreta.length;i++){ 
+            if(quantDados [vetDiscreta[i] ]){
+                quantDados[vetDiscreta[i]]+=1
+                acum++
+            }else{
+                quantDados[vetDiscreta[i]]=1
+                acum++
+            }			
+    }
+    for(var chave in quantDados){ 
+        tablequalitativa.innerHTML+= `<td> ${chave /*Inserção de variavel na tabela*/}  <td> ${quantDados[chave]/*Insere a quantidade de repetições da variavel*/} <td>${Math.round((quantDados[chave]/acum)*100) /*Insere e calcula a porcentagem de vezes que o elemento foi inserido*/} % <td> <br> `      
+}
 }
 function qualitativanominal(){
     tablequalitativa.innerHTML +="<tr> <td>VARIAVEL</td><td>FI</td><td>FR%   </td><td>FAC   </td><td>FAC%  </td></tr> "
-    var nomeVar = document.getElementById('variavelqualitativa').value; //Coloca o nome da variavel
-    var dados_nominal = document.getElementById('valor').value; //puxa os dados da pagina
-    let vetNominal = dados_nominal.toString().split(';'); // separa os valores e insere no vetor
-    vetNominal.sort() // deixa o vetor na ordem crescente/alfabetica
-     var quantDados ={} ; //obj com informando a quantidade de cada elemento 
-     var acum =0 ; // acumulador de elementos diferentes
-    nomeVariavel.innerHTML+=nomeVar
-        for(let i = 0 ; i<vetNominal.length;i++){ //metodo para alocar a quantidade de cada elemento
+    var nomeVar = document.getElementById('variavelqualitativa').value;
+    var dados_nominal = document.getElementById('valor').value;
+    let vetNominal = dados_nominal.toString().split(';');
+    vetNominal.sort() 
+     var quantDados ={} ; 
+     var acum =0 ; 
+     variavelquali.innerHTML+=nomeVar
+        for(let i = 0 ; i<vetNominal.length;i++){ 
             if(quantDados [vetNominal[i] ]){
                 quantDados[vetNominal[i]]+=1
                 acum++
@@ -69,11 +90,11 @@ function qualitativanominal(){
     var acumFac = 0;
     var factotPorc =0;
     var acumFacPorc = 0; 
-    for(var chave in quantDados){ // pega as variaveis
+    for(var chave in quantDados){ 
        fac+=quantDados[chave]
         facPorc = Math.round((quantDados[chave]/acum)*100)
         factotPorc = fac + acumFacPorc
-        tablequalitativa.innerHTML+= `<td> ${chave /*Inserção de variavel na tabela*/}  <td> ${quantDados[chave]/*Insere a quantidade de repetições da variavel*/} <td>${Math.round((quantDados[chave]/acum)*100) /*Insere e calcula a porcentagem de vezes que o elemento foi inserido*/} % <td> ${fac}<td> ${factotPorc}  <br> `
+        tablequalitativa.innerHTML+= `<td> ${chave}  <td> ${quantDados[chave]} <td>${Math.round((quantDados[chave]/acum)*100) } % <td> ${fac}<td> ${factotPorc}  <br> `
         acumFacPorc+=facPorc
     }
 }
@@ -81,26 +102,41 @@ function qualitativanominal(){
 
 
 function quantitativacontinua(){
+        var nomeVar = document.getElementById('variavelquant').value;
+        variavelquant.innerHTML+=nomeVar
+		var dados_continua = document.getElementById('valores').value;
+		let vetDiscreta = dados_continua.toString().split(';');
+		vetDiscreta.sort(ordemCrescente)
+        var n = vetDiscreta.length;
+        var xmin=vetDiscreta[0];
+        var xmax=vetDiscreta[n-1];
+        var k =Math.sqrt(n);
+        k=Math.floor(k)
+        console.log(k)
 
-    let adc = document.getElementById("valores").value;
-    let lista= adc.toString().split(';')//tira o ;
-    lista.sort(Crescente)//executa a função de organização
-
-    let xmax = lista[lista.length - 1];
-    let xmin = lista[0]
-    let at = xmax-xmin
-
-    let k = lista.length
-    k = (Math.sqrt(k))
-    let k2 =Math.floor(k)
-
-    console.log(k2,k)
-    console.log(lista)
+        var ic = 0 ; 
+        console.log(xmax+":"+xmin)
+        console.log(k);
+         var alt = xmax - xmin
+         console.log(alt)
+         ic = Math.floor(alt/k)
+        
+           console.log(alt+ "/"+k+" = "+ic)
+        var valorInicial = Number(xmin);
+        console.log(`Antes do while ${valorInicial}:${xmax}`)
+        console.log(typeof ic + typeof valorInicial)
+        while(valorInicial <= xmax){ 
+        var valorFinal = 0 ;
+        valorFinal = Number(valorInicial + ic)
+        console.log(`${valorInicial}:${valorFinal}:${xmax}`)
+        tablequantitativa.innerHTML+=`<td>${valorInicial}<td>-------------<td>${valorFinal}<td>` 
+        var valorInicial = valorFinal
+        }
     }
 
 
 function Crescente(a,b){
-    return(a-b)	// organiza
+    return(a-b)
 }
 
 function testeasa(){
