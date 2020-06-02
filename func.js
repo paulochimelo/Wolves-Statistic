56c824d3ea023cb7d72b1ffa7c3408b375164dfc
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-06-02 10:52:27 -0300

    Update func.js

--- func.js
+++ func.js
@@ -51,7 +51,7 @@ function qualitativaordinal(){
     tablequalitativa.innerHTML += `<tr> <td>${nomeVar}  </td><td>Frequência Simples</td> <td>Frequência Relativa</td><td>Frequência Acumulada</td><td>Frequência Acumulada Percentual</td></tr> `
     const dados = document.getElementById('valor').value; 
     const vetDiscreta = dados.toString().split(';'); 
-    vetDiscreta.sort(ordemCrescente) 
+    vetDiscreta.sort() 
     let quantDados ={} ; 
     let acum =0 ; 
     variavelquali.innerHTML+="Variavel Qualitativa Ordinal"
@@ -79,8 +79,18 @@ let fapcont = 0
     console.log(ultimo)
     let ele1 = vetDiscreta[0]
     console.log(ele1)
-    console.log(quantDados)
+    console.log(vetDiscreta)
     console.log(acum)
+    if (acum & 1){
+        console.log("Impar")
+        let metade = Math.round(acum/2)
+        let m = Number(Math.round(metade))
+        console.log(m)
+    } else {
+        console.log("Par")
+    }
+    let mediana = vetDiscreta[m]
+    console.log(mediana)
 }
 
 function qualitativanominal(){
