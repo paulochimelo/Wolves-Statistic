9830389a54feb9f9abe33fe4737df31d9ed8079f
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2020-06-09 10:48:53 -0300

    Medidas separatrizes qualitativa

--- func.js
+++ func.js
@@ -1,4 +1,10 @@
-
+function medidaseparatiz(){
+  console.log("true")
+  let opmax = document.getElementById('medidaseparatizes').value
+  console.log(opmax)
+  porcentagem.max = opmax
+  porcentagem.value = ''
+}
 
 function qualitativa(){
     let tipodado = document.getElementById('tipdadosqualitativa').value
@@ -78,6 +84,30 @@ let fapcont = 0
         tablequalitativa.innerHTML+= `<td> ${chave /*Inserção de variavel na tabela*/}  <td> ${quantDados[chave]/*Insere a quantidade de repetições da variavel*/} <td>${Math.round((quantDados[chave]/acum)*100) /*Insere e calcula a porcentagem de vezes que o elemento foi inserido*/} % <td> ${fac} <td> ${fap} % <br> ` 
           
     }
+//Medida separatriz
+    mdseparatriz.innerHTML = ''
+    let porce = document.getElementById('porcentagem').value
+    console.log(porce)
+    let pmax =  document.getElementById('porcentagem').max
+    console.log(pmax)
+    let porc = (100/pmax)*porce
+    console.log(porc)
+    let itemvet = Math.round((acum/100)*porc)
+    console.log(itemvet)
+    let separatrizes = vetDiscreta[itemvet-1]
+    console.log(separatrizes)
+    let returnid = ''
+    if (pmax == 4){
+      returnid = "Q"
+    }else if (pmax == 5){
+      returnid = "K"
+    }else if (pmax == 10){
+      returnid = "D"
+    }else if (pmax == 100){
+      returnid = "P"
+    }
+    mdseparatriz.innerHTML = `${returnid}${porce} = ${separatrizes}`
+    
      //fazer media moda mediana
      let moda = 0
      let ocorrenciasMaior = -1;
@@ -188,6 +218,29 @@ function qualitativanominal(){
        fap += fapcont
         tablequalitativa.innerHTML+= `<td> ${chave}  <td> ${quantDados[chave]} <td>${Math.round((quantDados[chave]/acum)*100) } % <td> ${fac}<td> ${fap} % <br> `
     }
+    //medida separatriz
+    mdseparatriz.innerHTML = ''
+    let porce = document.getElementById('porcentagem').value
+    console.log(porce)
+    let pmax =  document.getElementById('porcentagem').max
+    console.log(pmax)
+    let porc = (100/pmax)*porce
+    console.log(porc)
+    let itemvet = Math.round((acum/100)*porc)
+    console.log(itemvet)
+    let separatrizes = vetNominal[itemvet-1]
+    console.log(separatrizes)
+    let returnid = ''
+    if (pmax == 4){
+      returnid = "Q"
+    }else if (pmax == 5){
+      returnid = "K"
+    }else if (pmax == 10){
+      returnid = "D"
+    }else if (pmax == 100){
+      returnid = "P"
+    }
+    mdseparatriz.innerHTML = `${returnid}${porce} = ${separatrizes}`
      //fazer media moda mediana
      let moda = null
      let ocorrenciasMaior = -1;
@@ -394,7 +447,29 @@ let fapcont = 0
         fap += fapcont
         tablequalitativa.innerHTML+= `<td> ${chave /*Inserção de variavel na tabela*/}  <td> ${quantDados[chave]/*Insere a quantidade de repetições da variavel*/} <td>${Math.round((quantDados[chave]/acum)*100) /*Insere e calcula a porcentagem de vezes que o elemento foi inserido*/} % <td> ${fac} <td> ${fap} % <br> `      
     }
-
+    //medida separatriz
+    mdseparatriz.innerHTML = ''
+    let porce = document.getElementById('porcentagem').value
+    console.log(porce)
+    let pmax =  document.getElementById('porcentagem').max
+    console.log(pmax)
+    let porc = (100/pmax)*porce
+    console.log(porc)
+    let itemvet = Math.round((acum/100)*porc)
+    console.log(itemvet)
+    let separatrizes = vetNominal[itemvet-1]
+    console.log(separatrizes)
+    let returnid = ''
+    if (pmax == 4){
+      returnid = "Q"
+    }else if (pmax == 5){
+      returnid = "K"
+    }else if (pmax == 10){
+      returnid = "D"
+    }else if (pmax == 100){
+      returnid = "P"
+    }
+    mdseparatriz.innerHTML = `${returnid}${porce} = ${separatrizes}`
     //fazer media moda mediana
     let moda = null
     let ocorrenciasMaior = -1;
@@ -465,14 +540,68 @@ let fapcont = 0
         fap += fapcont
         tablequalitativa.innerHTML+= `<td> ${chave /*Inserção de variavel na tabela*/}  <td> ${quantDados[chave]/*Insere a quantidade de repetições da variavel*/} <td>${Math.round((quantDados[chave]/acum)*100) /*Insere e calcula a porcentagem de vezes que o elemento foi inserido*/} % <td> ${fac} <td> ${fap} % <br> `      
     }
-
+    //medidas separatrizes
+    mdseparatriz.innerHTML = ''
+    let porce = document.getElementById('porcentagem').value
+    console.log(porce)
+    let pmax =  document.getElementById('porcentagem').max
+    console.log(pmax)
+    let porc = (100/pmax)*porce
+    console.log(porc)
+    let itemvet = Math.round((acum/100)*porc)
+    console.log(itemvet)
+    let separatrizes = vetDiscreta[itemvet-1]
+    console.log(separatrizes)
+    let returnid = ''
+    if (pmax == 4){
+      returnid = "Q"
+    }else if (pmax == 5){
+      returnid = "K"
+    }else if (pmax == 10){
+      returnid = "D"
+    }else if (pmax == 100){
+      returnid = "P"
+    }
+    mdseparatriz.innerHTML = `${returnid}${porce} = ${separatrizes}`
     //fazer media moda mediana
-    let ultimo = vetDiscreta[vetDiscreta.length - 1]
+    let moda = 0
+    let ocorrenciasMaior = -1;
+
+    let contagem = 1;
+    for ( let i = 0 ; i <= vetDiscreta.length ; i++ ) {
+    if ( i < vetDiscreta.length && vetDiscreta[i] == vetDiscreta[i-contagem] )
+        contagem++;
+    
+    else if ( contagem > ocorrenciasMaior ) {
+        moda = vetDiscreta[i-1];
+        ocorrenciasMaior = contagem;
+    }
+    }
+    if (moda == undefined){
+       moda = String("Amodal")
+    }
+
+   let ultimo = vetDiscreta[vetDiscreta.length - 1]
     console.log(ultimo)
+    console.log(ultimo);
     let ele1 = vetDiscreta[0]
     console.log(ele1)
-    console.log(quantDados)
+    console.log(vetDiscreta)
     console.log(acum)
+    let m = Math.round(acum/2)
+    let mediana = null
+    if (acum & 1){
+       console.log("Impar")
+       console.log(m)
+       console.log("Media: Não Possui")
+       mediana = vetDiscreta[m-1]
+    } else {
+        console.log("Par")
+        console.log(m)
+        mediana = (vetDiscreta[m-1] +","+ vetDiscreta[m])
+    }
+   console.log(`Moda : ${moda}`)
+   tcqualitativa.innerHTML = ( `<tr> Mediana:${mediana} ||</tr> Moda: ${moda}`)
 }
 
 function quantitativadiscreta(){
